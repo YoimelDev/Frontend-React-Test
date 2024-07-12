ccb0efca458aced138a34928dce755a2bd395d71
YoimelDev <yoimeldev@gmail.com>
2024-07-12 18:41:20 -0400

    feat: Add information section and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 // Components
 import { TimeCountDown, Header, Hero, SocialMedia } from '@/components'
 
+// Images
+import pic1 from '@/assets/images/Pic1.webp'
+
 // Styles
 import '@/assets/scss/app.scss'
 import 'boxicons/css/boxicons.min.css'
@@ -14,6 +17,32 @@ const App = () => {
 		<SocialMedia />
 
 		<TimeCountDown />
+
+		<main>
+			<section className='information'>
+				<div className="information__container container">
+					<div className='information__content'>
+						<h2>Lorem ipsum dolor sit amet</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+							diam nonumy eirmod tempor invidunt ut labore et dolore
+							magna aliquyam erat, sed diam voluptua. At vero eos et
+							accusam et justo duo dolores et ea rebum. Stet clita kasd
+							gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+							amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+							sed diam nonumy eirmod tempor invidunt ut labore et dolore
+							magna aliquyam erat, sed diam voluptua. At vero eos et
+							accusam et justo duo dolores et ea rebum. Stet clita kasd
+							gubergren, no sea takimata sanctus est Lorem ipsum dolor sit												
+						</p>
+					</div>
+
+					<picture>
+						<img src={pic1} loading="lazy" alt="Imagen" />
+					</picture>
+				</div>
+			</section>
+		</main>
 	</>
 }
 
