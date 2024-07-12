313b47d8e28a6496a9284afb08dd59649c1b9ee8
YoimelDev <yoimeldev@gmail.com>
2024-07-12 19:41:37 -0400

    feat: Add horizon section and styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import pic1 from '@/assets/images/Pic1.webp'
 import pic2 from '@/assets/images/Pic2.webp'
 import pic3 from '@/assets/images/Pic3.webp'
 import pic4 from '@/assets/images/Pic4.webp'
+import pic5 from '@/assets/images/Pic5.webp'
+import green_bg from '@/assets/images/green_bg.webp'
 
 // Styles
 import '@/assets/scss/app.scss'
@@ -98,10 +100,35 @@ const App = () => {
 						</div>
 
 						<div className="learn__button">
-							<button className='btn btn--border'>
+							<button className="btn btn--border">
 								LEARN MORE
 							</button>
 						</div>
+					</div>
+				</section>
+
+				<section className="horizon">
+					<div className="horizon__left">
+						<div className="horizon__left__content">
+							<h2>Lorem ipsum dolor sit amet, consetetur</h2>
+
+							<button className="btn btn--white">
+								LOREM IPSUM DOLOR SIT
+							</button>
+							<button className="btn btn--border white">
+								LOREM IPSUM DOLOR SIT AMET
+							</button>
+						</div>
+					</div>
+
+					<div className="horizon__right">
+						<picture className="horizon__right--overlay">
+							<img src={green_bg} loading="lazy" alt="Imagen" />
+						</picture>
+
+						<picture>
+							<img src={pic5} loading="lazy" alt="Imagen" />
+						</picture>
 					</div>
 				</section>
 			</main>
