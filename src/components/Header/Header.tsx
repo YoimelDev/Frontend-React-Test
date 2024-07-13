df7b81057ff8b314b02203e2b3a44a1293e771a5
YoimelDev <yoimeldev@gmail.com>
2024-07-13 12:28:31 -0400

    feat: Add responsive to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import { AccessPointIcon, PlayerPlayIcon } from '@/components/Icons'
 
 import Logo from '@/assets/images/logo.webp'
 
 export const Header = () => {
+	const [isActive, setIsActive] = useState(false)
+
 	return (
 		<header className="header">
 			<div className="header__topBar">
@@ -24,15 +27,50 @@ export const Header = () => {
 				</div>
 
 				<nav className="header__nav">
-					<ul>						{
-							Array.from({ length: 5 }).map((_, index) => (
-								<li key={index}>
-									<a key={index} href="#">Lorem Ipsum</a>
-								</li>
-							))
-						}
+					<ul>
+						{Array.from({ length: 5 }).map((_, index) => (
+							<li key={index}>
+								<a key={index} href="#">
+									Lorem Ipsum
+								</a>
+							</li>
+						))}
 					</ul>
-				</nav>				
+				</nav>
+			</div>
+
+			<div className="header__bottomBar__mobile">
+				<div className='header__bottomBar__mobile__container'>
+					<div className="header__logo">
+						<img src={Logo} alt="logo" />
+					</div>
+
+					<button
+						className="header__bottomBar__mobile__menu"
+						onClick={() => setIsActive(!isActive)}
+					>
+						{isActive ? (
+							<i className="bx bx-x bx-lg"></i>
+						) : (
+							<i className="bx bx-menu bx-lg"></i>
+						)}
+					</button>
+				</div>
+
+				<nav
+					className={`header__nav ${isActive ? 'active' : ''}`}
+					onClick={() => setIsActive(false)}
+				>
+					<ul>
+						{Array.from({ length: 5 }).map((_, index) => (
+							<li key={index}>
+								<a key={index} href="#">
+									Lorem Ipsum
+								</a>
+							</li>
+						))}
+					</ul>
+				</nav>
 			</div>
 		</header>
 	)
